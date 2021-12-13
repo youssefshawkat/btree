02e14a4add1584e6f08163c465b3592453bf8903
youssefshawkat <68171635+youssefshawkat@users.noreply.github.com>
2021-12-13 23:29:22 +0200

    intial construction

--- main.cpp
+++ main.cpp
@@ -8,12 +8,38 @@ template <class T,int m>
 class BtreeNode{
 
 private:
-    bool root = false;
+    //bool root = false;
+    int order = m;
+    int NUM_K = 0;
     T* keys;
     BtreeNode** children;
+public:
+
+    BtreeNode(){
+
+        keys = new T[m-1];
+
+        children = new BtreeNode*[m];
+    }
+    //This function is for testing only and it's not accurate
+    void getKeys(){
+
+        for(int i = 0 ; i < m ; i++){
+
+            cout <<  keys[i] << endl;
 
 
+        }
 
+    }
+
+    ~BtreeNode(){
+
+        delete[] keys;
+        delete[] children;
+    }
+
+template<class T,int m> friend class BTree;
 
 
 
@@ -26,13 +52,25 @@ class BTree{
 
 private:
     int order;
+    BtreeNode<int,m> *root;
 
 public:
     BTree(){
-
+      root = nullptr;
         order = m;
+    }
+
+    void insert(T key){
+
+        if (root == nullptr){
+
+            root = new BtreeNode<T,m>;
+            root->keys[0] = key;
+            root->NUM_K++;
+        }
 
     }
+
 
 
 
@@ -42,6 +80,8 @@ public:
 
 int main() {
     BTree<int,3> t1;
+
+
 
 
     return 0;
